c47acf66506fa140374283b39b2cf84d815ce89f
Greg Rell <gregory.rell@universalorlando.com>
2024-03-27 07:35:15 -0400

    Routing working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import './App.css';
+import "./App.css";
 
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Header from './components/Header.jsx'
-import NotesListPage from './pages/NotesListPage.jsx';
-import NotePage from './pages/NotePage.jsx';
+import Header from "./components/Header.jsx";
+import NotesListPage from "./pages/NotesListPage.jsx";
+import NotePage from "./pages/NotePage.jsx";
 
 function App() {
   return (
     <>
-      <Header/>
-        <Router>
-          <Routes>
-            <Route path='/' exact Component={NotesListPage}/>
-            <Route path='note/:id' Component={NotePage}/>
-          </Routes>
-        </Router>
-    
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="/" exact Component={NotesListPage} />
+          <Route path="/note/:id" Component={NotePage} />
+        </Routes>
+      </Router>
     </>
- 
-
   );
 }
 
