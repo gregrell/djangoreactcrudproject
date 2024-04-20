bc088e3f819bb90d3c2a306d72fb09965fbdfca7
Greg Rell <gregory.rell@universalorlando.com>
2024-04-20 14:08:41 -0400

    form and submission to authentication api working

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useContext, createContext } from "react";
+import { useContext, createContext, useState } from "react";
 
 const authcontextdata = createContext(null);
 
@@ -7,8 +7,32 @@ export function useAuthContext() {
   return useContext(authcontextdata);
 }
 const AuthContext = ({ children, ...rest }) => {
-  const authcontextvalue = {
+  let [user, setUser] = useState();
+  let [auth, setAuth] = useState();
+
+  async function loginUser(username, password) {
+    let server1 = "https://www.boredapi.com/api/activity";
+    let server2 = "http://localhost:8000/api/token/";
+
+    const user = { username: username, password: password };
+
+    let response = await fetch(server2, {
+      method: "POST",
+      user,
+      headers: {
+        "content-type": "application/json",
+      },
+      withCredentials: true,
+      body: JSON.stringify(user),
+    });
+
+    const activity = await response.json();
+    console.log(activity);
+  }
+
+  let authcontextvalue = {
     name: "greg",
+    loginUser,
   };
 
   return (
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import { useContext } from "react";
 import { TheContext } from "../context/MyContext";
+import { useAuthContext } from "../context/AuthContext";
 
 const LoginPage = () => {
   const somecontext = useContext(TheContext);
+  const authcontext = useAuthContext();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const username = e.target.username.value;
+    const password = e.target.password.value;
+
+    if (username && password) {
+      authcontext.loginUser(username, password);
+    }
+    if (!username) {
+      alert("username is blank");
+    }
+    if (!password) {
+      alert("password is blank");
+    }
+  }
+
   return (
     <>
       <p>login page context {somecontext}</p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="username"
