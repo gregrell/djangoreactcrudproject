c0237eee2e0f836a04e371125bcc7951ebb8fd86
Greg Rell <gregory.rell@universalorlando.com>
2024-08-11 13:52:49 -0400

    Migrations and model changes. Added note requiring an author

--- tutorial/mainapp/admin.py
+++ tutorial/mainapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Note
+from .models import Note, UserInfo
 
 admin.site.register(Note)
+admin.site.register(UserInfo)
--- tutorial/mainapp/models.py
+++ tutorial/mainapp/models.py
@@ -1,13 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
+
+# Note represents a model of the content to be displayed
 class Note(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=False, blank=False, default=1
+    )
     body = models.TextField(null=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return self.body[0:50]
-    
 
+
+# Contains specific user information additional to the auth.models User model
+class UserInfo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.TextField(max_length=120, null=False, blank=False)
+    last_name = models.TextField(max_length=120, null=False, blank=False)
+    dob = models.DateField(null=False, blank=False)
+
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username[0:50]
