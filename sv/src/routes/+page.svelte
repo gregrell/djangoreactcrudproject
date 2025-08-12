06026fd50f62de10dff2cca2c1e575cb23e6c98f
Greg Rell <gregory.rell@universalorlando.com>
2025-08-12 08:46:11 -0400

    work on derived state, ts typing, optional props

--- sv/src/routes/+page.svelte
+++ sv/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang='ts'>
     import Header from "./Header.svelte";
-    let name = $state("Greg")
+    let name = $state("Greg"); // normal state rune
+    let full_name = $derived("Hello full name "+name); //derived state rune
     let status: 'OPEN' | 'CLOSED' = $state('OPEN');
 
 
@@ -11,11 +12,12 @@
 
 </script>
 
-<Header {name}/>
+<Header {name} fake_name="Wes"/>
 <input type='text' bind:value={name}/>
 
 <p>The store is now {status}</p>
 <button onclick={toggle}>Toggle Status</button>
+<p>{full_name}</p>
 
 
 
--- sv/src/routes/Header.svelte
+++ sv/src/routes/Header.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
-    let{name}  = $props();
+    let{
+        name,
+        fake_name = null}:{
+        name:string;
+        fake_name?: string|null}  = $props(); //props rune. setting types using typescript syntax. 
+                                        //if the variable is declared here, it becomes required. 
+                                        //if putting a question mark after it, it is optional
 </script>
 
 
-<h1>Hello  {name} from Header</h1>
+<h1>Hello  {name} and optionally hello {fake_name}</h1>
 
 <style>
 
