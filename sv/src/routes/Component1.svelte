6545ac1414cb7e38ce91773b9fadeabfbb8f8e14
Greg Rell <gregory.rell@universalorlando.com>
2025-08-13 08:18:07 -0400

    svelte is awesome.

--- sv/src/routes/Component1.svelte
+++ sv/src/routes/Component1.svelte
@@ -1,6 +1,7 @@
 <script>
 	let formState = $state({
 		name: '',
+		birthday: '',
 		step: 0,
 		errorMess: ''
 	});
@@ -11,21 +12,35 @@
 		{#if formState.step === 0}
 			<label for="name">Name:</label>
 			<input type="text" id="name" bind:value={formState.name} placeholder="Name" />
+		{:else if formState.step === 1}
+			<label for="birthday">Birthday:</label>
+			<input type="date" id="birthday" bind:value={formState.birthday} />
 		{/if}
 		<button onclick={() => formState.step--} disabled={formState.step === 0}>Previous</button>
-
-		<button
-			onclick={() => {
-				if (formState.name.trim() === '') {
-					formState.errorMess = 'Name cannot be empty';
-				} else {
-					formState.errorMess = '';
-					formState.step++;
-				}
-			}}>Next Step</button
-		>
+		{#if formState.step < 2}
+			<button
+				onclick={() => {
+					if (formState.name.trim() === '' && formState.step === 0) {
+						formState.errorMess = 'Name cannot be empty';
+					} else if (formState.birthday.trim() === '' && formState.step === 1) {
+						formState.errorMess = 'Birthday cannot be empty';
+					} else {
+						formState.errorMess = '';
+						formState.step++;
+					}
+				}}>Next Step</button
+			>
+		{/if}
 		<p>Current Step: {formState.step}</p>
-		<p>Error Message: {formState.errorMess}</p>
+		{#if formState.errorMess}
+			<p>Error Message: {formState.errorMess}</p>
+		{/if}
+
+		{#if formState.step === 2}
+			<p>Thank you for your submission!</p>
+			<p>Name: {formState.name}</p>
+			<p>Birthday: {formState.birthday}</p>
+		{/if}
 	</div>
 </main>
 
